4165e8c2ee570d46b1890953f47787da20974859
vadde suresh <sureshvadde1076@gmail.com>
2023-01-30 10:34:28 +0530

    student library

--- src/main/java/com/driver/services/BookService.java
+++ src/main/java/com/driver/services/BookService.java
@@ -27,20 +27,20 @@ public class BookService {
     }
 
     public List<Book> getBooks(String genre, boolean available, String author){
-        List<Book> books = null; //find the elements of the list by yourself
+        //find the elements of the list by yourself
 
         if(genre!=null && author != null){
-            books = bookRepository2.findBooksByGenreAuthor(genre,author,available);
+            return bookRepository2.findBooksByGenreAuthor(genre,author,available);
         }
         else if(genre!=null){
-            books = bookRepository2.findBooksByGenre(genre,available);
+            return bookRepository2.findBooksByGenre(genre,available);
         }
         else if(author!=null){
-            books = bookRepository2.findBooksByAuthor(author,available);
+            return bookRepository2.findBooksByAuthor(author,available);
         }
         else{
-            books = bookRepository2.findByAvailability(available);
+           return bookRepository2.findByAvailability(available);
         }
-        return books;
+
     }
 }
